55bad82f59598dffad722def445d2e28a7273f2e
Draco <draco@dracoli.com>
2023-12-10 12:49:33 -0500

    Replace rosetta import

--- server/start.go
+++ server/start.go
@@ -45,8 +45,8 @@ import (
 	"github.com/cometbft/cometbft/proxy"
 	"github.com/cometbft/cometbft/rpc/client/local"
 
-	"github.com/cosmos/cosmos-sdk/server/rosetta"
-	crgserver "github.com/cosmos/cosmos-sdk/server/rosetta/lib/server"
+	"cosmossdk.io/tools/rosetta"
+	crgserver "cosmossdk.io/tools/rosetta/lib/server"
 
 	ethmetricsexp "github.com/ethereum/go-ethereum/metrics/exp"
 
